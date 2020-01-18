d1217852b0ad2eb4ea6377ec8bea9539cb978b81
EC2 Default User <rayatanu31@gmail.com>
2020-01-18 21:21:50 +0000

    Added code to trigger CodeReviewer

--- MyDummyCode.java
+++ MyDummyCode.java
@@ -22,5 +22,15 @@ puclic class MyDummyCode {
         
         	// Displaying the multiplication result
         	System.out.println("Output: "+product);
+
+		try {
+            		FileWriter myWriter = new FileWriter("filename.txt");
+            		myWriter.write("Files in Java might be tricky, but it is fun enough!");
+            		// myWriter.close();
+            		System.out.println("Successfully wrote to the file.");
+        	} catch (IOException e) {
+            		System.out.println("An error occurred.");
+            		e.printStackTrace();
+        	}
 	}
 }
