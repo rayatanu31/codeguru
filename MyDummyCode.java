2645d898be47d7a4d1d2cf87889fa83a4f2b56d9
EC2 Default User <rayatanu31@gmail.com>
2020-01-19 02:36:51 +0000

    Bug Fix

--- MyDummyCode.java
+++ MyDummyCode.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
+import java.io.FileWriter;   // Import the FileWriter class
+import java.io.IOException;  // Import the IOException class to handle errors
+import java.io.File;  // Import the File class
 
-puclic class MyDummyCode {
+public class MyDummyCode {
 	public static void main(String[] abc) {
         	/* This reads the input provided by user
          	* using keyboard
