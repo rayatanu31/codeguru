2364884188e27b81277aeb06c3766a16076736af
EC2 Default User <rayatanu31@gmail.com>
2020-01-20 06:48:37 +0000

    Adding TrialBench

---
+++ TrialBench.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+
+public class TrialBench extends Thread {
+	static HashMap<Integer, String> h = new HashMap<Integer, String>();
+
+
+	public void run() {
+		try {
+			Thread.sleep(1000);
+			h.put(4, "D");
+			System.out.println("D");
+		}
+		catch(InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] abc) throws InterruptedException{
+		h.put(1,"A");
+		h.put(2,"B");
+		h.put(3,"C");
+		TrialBench tb = new TrialBench();
+		tb.start();
+
+	}
+}
