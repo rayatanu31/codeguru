4b55043d6ad193f9f66ddacf9ebedfd65a9065d2
EC2 Default User <rayatanu31@gmail.com>
2020-01-21 07:35:27 +0000

    Adding Maven Ptoject

---
+++ my-app/src/main/java/com/innovate/app/NewTrialBench.java
@@ -0,0 +1,45 @@
+package com.innovate.app;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+
+
+public class NewTrialBench {
+
+	private static ConcurrentHashMap<Integer, String> h = new ConcurrentHashMap<Integer, String>();
+
+  public String get(Integer key) {
+          if(h.containsKey(key)) {
+                  return h.get(key);
+          }
+          return "None";
+  }
+
+
+	public void put(Integer key, String value) {
+		/*
+		synchronized(h) {
+			String obj = h.get(key);
+			if(obj == null) {
+				obj = value;
+				h.put(key,obj);
+			}
+		}*/
+		h.putIfAbsent(key, value);
+	}
+
+//	public getFromDDB() {
+//	}
+
+	public static void main(String[] abc) throws InterruptedException{
+		h.put(1,"A");
+		h.put(2,"B");
+		h.put(3,"C");
+		NewTrialBench ntb = new NewTrialBench();
+		System.out.println(ntb.get(1));
+		ntb.put(4,"TESTING TESTING");
+		System.out.println(ntb.get(4));
+
+	}
+}
