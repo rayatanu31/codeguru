e0c5cd1abd8051a9e79e85e1ccff93ffdf66973c
EC2 Default User <ec2-user@ip-172-31-27-74.ec2.internal>
2020-01-30 12:50:35 +0000

    Replace synchronize with putIfAbsent

--- my-app/src/main/java/com/innovate/app/Run1.java
+++ my-app/src/main/java/com/innovate/app/Run1.java
@@ -4,12 +4,14 @@ import java.util.concurrent.ConcurrentHashMap;
 public class Run1 {
 	private static ConcurrentHashMap<Integer,String> h = new ConcurrentHashMap<Integer,String>();
 	public void putValue(Integer key, String value) {
+		/*
 		synchronized(h) {
 			String val = h.get(key);
 			if(val == null) {
 				h.put(key, value);
 			}
-		}
+		}*/
+		h.putIfAbsent(key, value);
 	}
 	
 	public static void main(String[] args) {
